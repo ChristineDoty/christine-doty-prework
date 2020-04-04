8884cda0315fc8cb73167e9f71f9fd657e993309
ChristineDoty <christinedoty@gmail.com>
2020-04-04 17:53:21 -0500

    most of it works?

--- Module-2_Assessment/assets/javascript/game.js
+++ Module-2_Assessment/assets/javascript/game.js
@@ -1,47 +1,48 @@
 // Make List of words
-    const gods = ['hermes' , 'apollo', 'zeus', 'hera' , 'hades'];
-    let currentWord = [];
-    const guessedLetters = [];
-    const placeholders = [];
+const gods = ['hermes' , 'apollo', 'zeus', 'hera' , 'hades'];
+let currentWord = [];
+const guessedLetters = [];
+const placeholders = [];
+let wins = 0;
+let remainingGuesses = 10;
+let blanks = document.querySelector('#blanks');
+let lettersGuessed = document.querySelector('#lettersGuessed');
+let guesses = document.querySelector('#guesses');
+ 
 
-   
 // Choose a random word
 currentWord = gods[Math.floor(Math.random() * gods.length)];
 console.log (currentWord);
-            
-let remainingGuesses = 15
-let guesses = document.querySelector('#guesses');
 
-guesses.innerHTML = remainingGuesses;
-
- // Change number of underscores to match length of word
-let blanks = document.querySelector('#blanks');
-        
+ // Change number of underscores to match length of word    
     for (let i = 0; i < currentWord.length; i++) {
-            placeholders[i] = " _ "
+            placeholders[i] = "_"
             blanks.textContent = placeholders;
-        }
-        // User presses key to start
+    }
 
 
 // Listen for key press
-    const letter = function (event) {
-    const key = event.key;
+    document.addEventListener("keyup", function(event) {
+        const key = event.key;
+    //Remove 1 guess
+        remainingGuesses--;
+        guesses.innerHTML = remainingGuesses;
+
     // Compare key press to letters in word
     // If letter is in the word, reveal it
+
         for (let i = 0 ; i < currentWord.length; i++) {
             if (currentWord [i] === key) {
                 placeholders [i] = key;
                 console.log (placeholders);
                 blanks.innerHTML = placeholders.join(" ");
-            }
+                // Add letter to letters guessed
+                guessedLetters [i] = key;
+                lettersGuessed.innerHTML = guessedLetters.join(" "); 
+            } 
         }
-    }    
+    });
 
-document.addEventListener('keyup', letter);
-
-
-// Add letter to letters guessed
 
 // If user guesses word before tries run out, add 1 to score, If not, display "You lose"
 
