35ac34445948f62950013248fd950f1d1294df25
ChristineDoty <christinedoty@gmail.com>
2020-04-05 18:34:22 -0500

    js complete

--- Module-2_Assessment/assets/javascript/game.js
+++ Module-2_Assessment/assets/javascript/game.js
@@ -3,7 +3,6 @@ const maxGuesses = 10;
 let currentWord = [];
 let wins = 0;
 let remainingGuesses = 0;
-gameOver = false;
 let placeholders = [];
 let guessedLetters = [];
 let blanks = document.querySelector('#blanks');
@@ -11,11 +10,8 @@ let lettersGuessed = document.querySelector('#lettersGuessed');
 let guesses = document.querySelector('#guesses');
 let winsNumber = document.querySelector('#winsNumber');
 
-//Press key to start
-// document.addEventListener("keyup", game());
-gameSetup();
 
-function gameSetup (){
+let startGame = function (){
     //Choose a random word
     currentWord = gods[Math.floor(Math.random() * gods.length)];
     console.log (currentWord);
@@ -36,24 +32,26 @@ function gameSetup (){
 };
 
 //Main game stuff
+startGame();
 //Listen for key press
-this.addEventListener("keyup", function(event) {
+document.addEventListener("keyup", function(event) {
     //Check if the game is over and reset if it is
-        guessing(event.key);
-});
+        key = event.key
+        guessing(key);
+},false);
 
 function guessing () {
     if (remainingGuesses > 1 && placeholders.indexOf('_') != -1) {
-        if (guessedLetters.indexOf(event.key) == -1) {
-        guessedLetters.push(event.key);
+        if (guessedLetters.indexOf(key) == -1) {
+        guessedLetters.push(key);
         lettersGuessed.innerHTML = guessedLetters.join(' ');
         //remove 1 guess
         remainingGuesses--;
         }
         guesses.innerHTML = remainingGuesses;    
             for (let k = 0 ; k < currentWord.length; k++) {
-                if (currentWord [k] === event.key) {
-                placeholders [k] = event.key;
+                if (currentWord [k] === key) {
+                placeholders [k] = key;
                 console.log (placeholders);
                 blanks.innerHTML = placeholders.join(' ');
                 console.log(placeholders);
@@ -63,11 +61,11 @@ function guessing () {
     } else if (placeholders.indexOf('_') == -1){
             youWin();
     } else {
-        gameSetup ();
+        startGame();
     }
 };
-function youWin () {
+function youWin() {
     wins++;
     winsNumber.innerHTML = wins;
-    gameSetup ();
+    startGame();
 }
