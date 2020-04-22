b11f82c4cc8542cb98478f565aa91922ed1e1e53
ChristineDoty <christinedoty@gmail.com>
2020-04-22 14:29:24 -0500

    just js guessing game project

--- JavaScript_Basics_Assessment/activity-1.js
+++
@@ -1,6 +0,0 @@
-9 * 9;
-56 / 12;
-281 % 9;
-19 / 0;
-'56' + '92'; //This would concatenate the strings
-42 === 52
--- JavaScript_Basics_Assessment/activity-2.js
+++
@@ -1,7 +0,0 @@
-let firstName = prompt ('What is your name?');
-
-if (firstName.length > 4) {
-    alert ('Your name is greater than 4 characters');
-}else {
-    alert ('Your name is less than 4 characters.');
-}
